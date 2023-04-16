236af00072a65729da50a42d546e78a6ffa713d3
Michael Bennett <tun78940@temple.edu>
2023-04-16 02:02:38 -0400

    Fixed many bugs and change structure. The project now allows for multiple client connections and updated the architecture of the code base.

--- src/Client.java
+++ src/Client.java
@@ -2,16 +2,70 @@
 import java.io.*;
 import java.net.*;
 import java.util.concurrent.TimeUnit;
- 
+
 public class Client {
     // initialize socket and input output streams
     private Socket socket = null;
-    private DataInputStream input = null;
     private DataOutputStream out = null;
     private int ClientId;
+    private static int currentclients = 0;
+    private Server ServerOwner;
+    private String address;
+    private int port;
+    private ChatView view;
+
     // constructor to put ip address and port
-    public Client(String address, int port) throws IOException {
-        socket = new Socket(address, port);
+    public Client(String address, int port, ChatView view)
+    {
+        ClientId = currentclients; //Each client has a unique incremental Idw
+        currentclients++;
+        ServerOwner = Server.ThisServer;
+        this.address = address;
+        this.port = port;
+        // establish a connection
+        System.out.println("current clientid:  " + ClientId );
+        this.view = view;
+        view.show();
     }
 
+    public void newConnection(){
+        try {
+            socket = new Socket(address, port);
+            System.out.println("Connected");
+
+            // sends output to the socket
+            out = new DataOutputStream(socket.getOutputStream());
+
+            while(true){
+                // takes input from view
+                String input = view.sentText;
+
+                if(!input.isEmpty()){
+                    out.writeUTF(input);
+                    out.flush();
+                    view.sentText = "";
+                }
+                // read input from the server
+                DataInputStream in = new DataInputStream(socket.getInputStream());
+                if (in.available() > 0) {
+                    Message response = new Message(in.readUTF(), 1, "Server");
+                    view.appendMessage(response + "\n");
+                }
+
+                // wait a short amount of time before checking again
+                TimeUnit.MILLISECONDS.sleep(10);
+            }
+        } catch (IOException | InterruptedException u) {
+            u.printStackTrace();
+        }
+    }
+
+    public int getClientId(){
+        return ClientId;
+    }
+
+    public void requestGroup(int[] targetids){
+        // ServerOwner.requestConnection(ClientId,targetids);
+        ServerOwner.createChatRoom();
+    }
 }
